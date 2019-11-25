a63726c62470eae57d4996648ec89d16b7d30e81
Sergey Zhuravlev <Sergey4Zhuravlev@gmail.com>
2019-11-25 03:52:08 +0300

    data provider failed

---
+++ DataAggregator/DataAggregator/DataProvider.fsx
@@ -0,0 +1,16 @@
+﻿open System
+//open FSharp.Data.Sql
+open FSharp.Linq
+
+(*System.IO.Directory.SetCurrentDirectory (__SOURCE_DIRECTORY__)
+
+#r "../../packages/FSharp.SqlProvider/lib/net40/FSharp.Data.Sqlprovider.dll"
+
+[<Literal>]
+let dbModelConnectionString  = "C:\Sergey\Projects\NLP\Data\Words.sqlite"
+
+type Sql = SqlDataProvider<Common.DatabaseProviderTypes.SQLITE,
+                           ConnectionString = dbModelConnectionString,
+                           CaseSensitivityChange = Common.CaseSensitivityChange.ORIGINAL>*)
+
+
--- DataAggregator/DataAggregator/Program.fs
+++ DataAggregator/DataAggregator/Program.fs
@@ -1,6 +1,19 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open FSharp.Data.Sql
+open FSharp.Linq
+
+//System.IO.Directory.SetCurrentDirectory (__SOURCE_DIRECTORY__)
+
+
+[<Literal>]
+let dbModelConnectionString  = "Data Source=" + __SOURCE_DIRECTORY__ + @"\..\..\Data\Words.sqlite"
+
+type Sql = SqlDataProvider<Common.DatabaseProviderTypes.SQLITE,
+                           SQLiteLibrary = Common.SQLiteLibrary.SystemDataSQLite,
+                           ConnectionString = dbModelConnectionString,
+                           CaseSensitivityChange = Common.CaseSensitivityChange.ORIGINAL>
 
 [<EntryPoint>]
 let main argv =
